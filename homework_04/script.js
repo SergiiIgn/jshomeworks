1a9b01518bac2d3a7f5bdd43b97a995c8eb46c9d
SergiiIgn <110237022+SergiiIgn@users.noreply.github.com>
2022-11-19 14:58:17 +0200

    Add files via upload

--- homework_04/script.js
+++ homework_04/script.js
@@ -3,59 +3,151 @@
 // Task 1 Comparison
 
 // first option of function
-function areEqual(a = 0, b = 0) {
-    return a === b;
+function areEqual() {
+    let firstComparedNumber = prompt('Give the first number to compare');
+    let secondComparedNumber = prompt('Give the second number to compare');
+
+    alert('a = b ' + (firstComparedNumber === secondComparedNumber));
+    return firstComparedNumber === secondComparedNumber;
 }
 
 // second option of function
-const areEqual2 = (a = 0, b = 0) => a === b;
+const areEqual2 = () => {
+    
+    let firstComparedNumber = prompt('Give the first number to compare');
+    let secondComparedNumber = prompt('Give the second number to compare');
+    
+    alert('a = b ' + (firstComparedNumber === secondComparedNumber));
+    return firstComparedNumber === secondComparedNumber;
+}
 
-let firstComparedNumber = prompt('Give the first number to compare');
-let secondComparedNumber = prompt('Give the second number to compare');
-
-console.log('a = b ', areEqual(firstComparedNumber, secondComparedNumber));
-console.log('a = b ', areEqual2(firstComparedNumber, secondComparedNumber));
+console.log('areEqual', areEqual());
+console.log('areEqual2', areEqual2());
 
 //End of task 1
 //Task 2 Sum
 
 // first option of function
-function sum(a = 0, b = 0) {
-    return a + b;
+function sum() {
+    let firstTerm = Number(prompt('Give the first term to sum'));
+
+    if (!firstTerm) {
+        alert('Oops!!! It is not number');
+        return(false);
+
+    } else {
+        let secondTerm = Number(prompt('Give the second term to sum'));
+
+        if (!secondTerm) {
+            alert('Oops!!! It is not number');
+            return(false);
+
+        } else {
+            alert(firstTerm + ' + ' + secondTerm + ' = ' + (firstTerm + secondTerm));
+            return firstTerm + secondTerm;
+        }
+    }
 }
 
 // second option of function
-const sum2 = (a = 0, b = 0) => a + b;
+const sum2 = () => {
+    let firstTerm = Number(prompt('Give the first term to sum'));
 
-let firstTerm = Number(prompt('Give the first term to sum'));
-let secondTerm = Number(prompt('Give the second term to sum'));
+    if (!firstTerm) {
+        alert('Oops!!! It is not number');
+        return(false);
 
-console.log('a + b =', sum(firstTerm, secondTerm));
-console.log('a + b =', sum2(firstTerm, secondTerm));
+    } else {
+        let secondTerm = Number(prompt('Give the second term to sum'));
+
+        if (!secondTerm) {
+            alert('Oops!!! It is not number');
+            return(false);
+            
+        } else {
+            alert(firstTerm + ' + ' + secondTerm + ' = ' + (firstTerm + secondTerm));
+            return firstTerm + secondTerm;
+        }
+    }
+}
+
+console.log('a + b =', sum());
+console.log('a + b =', sum2());
 
 //End of task 2
 //Task 3 Exponentiation
 
-
 // first option of function
-function exponentiation(a = 0, b = 2) {
-    return a ** b;
+function exponentiation() {
+    base = prompt('Give a base');
+
+    if (base === null) {
+        alert('Task canceled');
+        return(false);
+
+    } else if (!Number(base) && (base !== '0')) {
+        alert('Oops!!! It is not number');
+        return(false);
+
+    } else {
+        power = prompt('Give a power');
+
+        if (power === null) {
+            alert('Task canceled');
+            return(false);
+
+        } else if (power == '') {
+            power = 2;
+
+        } else if (!Number(power)) {
+            alert('Oops!!! It is not number');
+            return(false);
+        }
+    }
+    alert(base + ' to the ' + Number(power) + ' power is ' + (base ** power));
+    return base ** power;
 }
 
 // second option of function
-const exponentiation2 = (a = 0, b = 2) => a ** b;
+const exponentiation2 = () => {
+    base = prompt('Give a base');
 
-let base = Number(prompt('Give a base'));
-let power = Number(prompt('Give a power'));
+    if (base === null) {
+        alert('Task canceled');
+        return(false);
 
-console.log(base, 'to the', power, 'power is', exponentiation(base, power));
-console.log(base , 'to the', power, 'power is', exponentiation(base, power));
+    } else if (!Number(base) && (base !== '0')) {
+        alert('Oops!!! It is not number');
+        return(false);
+
+    } else {
+        power = prompt('Give a power');
+
+        if (power === null) {
+            alert('Task canceled');
+            return(false);
+
+        } else if (power == '') {
+            power = 2;
+
+        } else if (!Number(power)) {
+            alert('Oops!!! It is not number');
+            return(false);
+        }
+    }
+    alert(base + ' to the ' + Number(power) + ' power is ' + (base ** power));
+    return base ** power;
+} 
+
+exponentiation();
+exponentiation2();
 
 //End of task 3
 //Task 4 Day of week 
 
-function weekDay(a) {
-    switch(a) {
+function weekDay() {
+    let dayNumber = prompt('Give a number of week day');
+    switch(dayNumber) {
         case '1':
             result = 'Monday';
             break;
@@ -80,12 +172,11 @@ function weekDay(a) {
         default:
             result = 'There is no such day';
     }
+    alert(result);
     return result;
 }
 
-let dayNumber = prompt('Give a number of week day');
-
-console.log(weekDay(dayNumber));
+console.log(weekDay());
 
 //End of task 4
 //Task 5 Greeting type selection
@@ -94,6 +185,11 @@ function greetingType() {
     
     let name = prompt('What is your name?');
     let age = Number(prompt('How old are you?'));
+    let greeting = 'Hello'
+
+    if (name == '') {
+        name = 'Gest';
+    }
 
     if (age > 30 ) {
         alert('Hello, ' + name);
@@ -102,6 +198,6 @@ function greetingType() {
     }
 }
 
-greetingType();
+console.log(greetingType());
 
 //End of task 5
